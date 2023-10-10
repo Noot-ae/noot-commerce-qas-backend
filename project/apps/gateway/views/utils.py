f8c8e998ddfafb2f38568093e7226770fdb864c3
mohnas <mn9142001@gmail.com>
2023-10-10 15:30:50 +0300

    added total factors

--- project/apps/gateway/views/utils.py
+++ project/apps/gateway/views/utils.py
@@ -2,8 +2,6 @@ from rest_framework.views import APIView
 from utils.cache import get_base_currency_factors
 from utils.mixins import CacheResponseMixin
 from rest_framework.response import Response
-from user.models import Region
-import logging
 
 
 class GetCurrencyFactorsView(CacheResponseMixin, APIView):
@@ -13,31 +11,9 @@ class GetCurrencyFactorsView(CacheResponseMixin, APIView):
     
     def get(self, request):
         return Response(
-            self.get_factors(
-                get_base_currency_factors(self.default_currency_code)
-            )
+            get_base_currency_factors(self.default_currency_code)
         )
 
-    def get_active_currency_codes(self) -> list[str]:
-        active_currencies = list(
-            Region.objects.all().distinct('currency_code').values_list('currency_code', flat=True)
-        )
-        return active_currencies
-        
-    def get_factors(self, _factors : dict) -> list[dict]:
-        factors = []
-        for code in self.get_active_currency_codes():
-            try:
-                factors.append(
-                    {
-                        "code" : code,
-                        "factor" : _factors.get(self.default_currency_code, {})[code.lower()]
-                    }           
-                )
-            except KeyError as e:
-                logging.warning(f"invalid code {code}")
-        return factors
-    
     @property
     def default_currency_code(self) -> str:
         return self.request.tenant.currency_code.lower()
